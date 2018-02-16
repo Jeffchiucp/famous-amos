04a95d69e10412caf2c7e36465472a8d7f3a8308
Jeffchiucp <jeffchiu2000@gmail.com>
2018-02-16 13:44:27 -0800

    pushed nodemailer

---
+++ routes/email.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer')
+const mailgunTransport = require('nodemailer-mailgun-transport')
+
+//http://jeffmcmahan.info/blog/how-to-use-nodemailer-with-mailgun/
+// stackoverflow link:
+//https://stackoverflow.com/questions/26956251/sending-emails-using-mailgun-with-nodemailer-package
+const mailgunOptions = {
+  auth: {
+    api_key: process.env.MAILGUN_ACTIVE_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  }
+};
+
+const transport = mailgunTransport(mailgunOptions);
+
+
+var nodemailerMailgun = nodemailer.createTransport(mailgunTransport(auth));
+
+nodemailerMailgun.sendMail({
+  from: 'myemail@example.com',
+  to: 'recipient@domain.com', // An array if you have multiple recipients.
+  subject: 'Hey you, awesome!',
+  text: 'Mailgun rocks, pow pow!',
+}, function (err, info) {
+  if (err) {
+    console.log('Error: ' + err);
+  }
+  else {
+    console.log('Response: ' + info);
+  }
+});
+
+module.exports = new nodemailerMailgun.sendMail();
